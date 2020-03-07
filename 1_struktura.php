b505e45faff40fb7910335313063eb72c11d5136
robercikzbostonu <robertnaw79@gmail.com>
2020-03-07 09:34:13 +0100

    first file

---
+++ 1_struktura.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <title>Struktura</title>
+  </head>
+  <body>
+    <?php
+$name="Janusz";
+$surname="Nosacz"; // $ zmienna
+echo $name,$surname;
+//herdoc
+
+echo <<<show
+<hr>
+Imię: $name<br>
+Nazwisko: $surname
+<hr>
+show;
+echo: $text;
+
+show;
+
+
+     ?>
+
+  </body>
+</html>
